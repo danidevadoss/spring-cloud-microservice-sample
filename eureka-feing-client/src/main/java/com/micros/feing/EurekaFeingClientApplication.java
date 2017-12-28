1046ea9db30ae7af55c9b2b9bc6070c80d97f3b6
STG\danield <danield@stgit.com>
2017-12-28 14:00:55 +0530

    Added hystrix dashboard

--- eureka-feing-client/src/main/java/com/micros/feing/EurekaFeingClientApplication.java
+++ eureka-feing-client/src/main/java/com/micros/feing/EurekaFeingClientApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class EurekaFeingClientApplication {
 
 	public static void main(String[] args) {
